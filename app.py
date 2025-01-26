884fee030ee8959568727866ee55422943d68031
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2025-01-26 08:19:43 +0100

    fonction load_data

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve
 from sklearn.metrics import precision_score, recall_score
 
 st.title("Application Web de Classification Binaire")
@@ -14,3 +14,6 @@ st.sidebar.title("Application Web de Classification Binaire")
 st.markdown("Vos champignons sont-ils comestibles ou toxiques ? 🍄")
 st.sidebar.markdown("Vos champignons sont-ils comestibles ou toxiques ? 🍄")
 
+def load_data():
+    data = pd.read_csv("data/mushrooms.csv")
+    return data
